453c9cf320301764b56ffa9fa59634560c87f5b4
dorianwolf <dorian.wolf@gmail.com>
2015-11-25 17:16:23 +0000

    finished the backend

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,40 @@
-# Homepage (Root path)
+require 'faker'
+require 'json'
+CONTACTS = []
+
+numbers = Random.rand(2)
+
+100.times do
+  name = Faker::Name.name
+  CONTACTS << {
+  name: name,
+  email: Faker::Internet.email(name)
+}
+end
+
+
 get '/' do
   erb :index
+end
+
+get '/contacts' do
+  # @contacts = Card.all
+  # @contacts.to_json
+  CONTACTS.to_json
+end
+
+post '/contacts/new' do
+  # request.body.rewind
+  # contact = JSON.parse request.body.read
+
+  CONTACTS << {name: params[:name], email: params[:email]}
+  CONTACTS.to_json
+end
+
+delete '/contacts/:id' do
+  # do something
+end
+
+put '/contacts/:id' do
+  # do something
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $.getJSON("/contacts", function(contacts){
+    $(contacts).each(function(i, contact){
+      $('#contact').append(
+        `<tr>
+          <td>${contact.name}</td><td>${contact.email}</td>
+        </tr>`
+      );
+    });
+  });
+
+  $('#button').click(function(event){
+
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var data = {"name": name, "email": email};
+
+    $.post('/contacts/new', data, function(contacts){
+      $(contacts).each(function(i, contact){
+        $('#contact').append(
+          `<tr>
+            <td>${contact.name}</td><td>${contact.email}</td>
+          </tr>`
+        );
+      });
+    }, 'json');
+  });
+
 });
